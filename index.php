bd76386a5516b7d3a90a1e2bf0dd14c7880af539
Catalina-R <stan.catalina@gmail.com>
2019-06-15 18:36:07 +0300

    modificari

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 
-// include 'index.html';
-// include 'lectii.php';
-// echo '<br>';
-// echo '<br>';
-// include 'functions.php';
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-error_reporting(0);
-
-phpinfo();
-$una = 'scrie ceva';
-echo '123';
+include 'index.html';
+include 'lectii.php';
+echo '<br>';
+echo '<br>';
+include 'functions.php';
