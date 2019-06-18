e1a4b3c5b3b4637258d8f5e5064982f4791a22b0
Catalina-R <stan.catalina@gmail.com>
2019-06-18 21:22:02 +0300

    variants

--- convert.php
+++ convert.php
@@ -2,31 +2,21 @@
 
 if(isset($_POST['binary'])){
     $code = $_POST['binary'];
-    //if(preg_match("/~^[01]+$~/", $code)) { 
-    //        echo "The decimal value is " . bindec($code) . "\n";
+}
+$arr = str_split($code);
+echo "<br/>";
+$gresit=false; 
+
+    foreach($arr as $number){
+
+        if ($number>'1'){
+            $gresit=true;
+        }        
+    }    
+
+if ($gresit==true){
+    echo "Please provide a valid binary!<br/>";
+}else{
+    echo "The decimal value is " . bindec($code) . "<br/>\n";
+}
     
-    //} else {
-    // echo "Please provide a valid binary!";
-    //}
-    
-    $acceptSet="01";
-    if(strspn($code,$acceptSet,0) !== strlen($code)){
-        echo "Please provide a valid binary!<br/>";
-    }else{
-        echo "The decimal value is " . bindec($code) . "<br/>\n";
-    }
-  }
-  
-  //echo"<h1>chestii:</h1>";
-  //var_dump($code);
-    // $arr = str_split($code);
-    // //print_r($arr);
-    // echo "<br/>";
-    // foreach($arr as $number){
-    //     if($number === '0' || $number === '1'){
-    //         echo "The decimal value is " . bindec($code) . "<br/>\n"; 
-    //     }else{
-            
-    //         echo "Please provide a valid binary!<br/>"; 
-    //     }
-    // }
